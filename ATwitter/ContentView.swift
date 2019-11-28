f08672fb4482a2f5bad8ba729ae17d577c69e81a
Asgedom <asmow@yahoo.com>
2019-11-27 23:13:18 -0600

    Atwitter clone account created using fireBase

--- ATwitter/ContentView.swift
+++ ATwitter/ContentView.swift
@@ -7,13 +7,71 @@
 //
 
 import SwiftUI
+import Firebase
+import SDWebImageSwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            Home().tabItem{
+                Image("Home")
+            }.tag(0)
+            Text("Search").tabItem{
+                Image("Search")
+            }.tag(1)
+            Text("Notifications").tabItem{
+                Image("Notifications")
+            }.tag(2)
+            Text("Messages").tabItem{
+                Image("Messages")
+            }.tag(3)
+            
+        }
+        .accentColor(.blue)
+        .edgesIgnoringSafeArea(.top)
+        
     }
 }
-
+struct Home: View {
+    var body: some View {
+        NavigationView{
+            ZStack {
+                List(0..<15){i in
+                    Text("Tweets\(i)")
+                }
+                VStack{
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image("Tweet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .padding()
+                        }
+                        .background(Color("bg"))
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                    }.padding()
+                }
+            }
+            .navigationBarTitle("Home",displayMode: .inline)
+            .navigationBarItems(leading:
+                Image("Tweet")
+                    .resizable()
+                    .frame(width: 35, height: 35)
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        print("slide out menu....")
+                        
+                }
+            )
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
